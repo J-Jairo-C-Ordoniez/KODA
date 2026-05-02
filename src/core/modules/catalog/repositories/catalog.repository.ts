39b0ac5bac4545d0aced242a503d68c73cf01f50
J-Jairo-C-Ordoniez <cordobaojhonjairo21@gmail.com>
2026-05-01 21:39:59 -0500

    KODA

--- src/core/modules/catalog/repositories/catalog.repository.ts
+++ src/core/modules/catalog/repositories/catalog.repository.ts
@@ -1,5 +1,5 @@
 import prisma from '@/infrastructure/db/client';
-import { Prisma } from '@prisma/client';
+import { type Prisma } from '@prisma/client';
 
 const catalogRepository = {
   async getCategories(tenantId: string) {
